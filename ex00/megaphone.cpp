6d4f41f2ef3a488ba39a194369b7c836f550be1f
ansimonn <ansimonn@student.42angouleme.fr>
2026-07-18 23:40:15 +0200

    ex00 completed

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <cctype>
 
 int main(const int ac, char **av)
 {
 	if (ac < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		++av;
 		while (*av)
 		{
-			std::cout << std::uppercase << *av << " ";
+			while (**av)
+			{
+				std::cout << (char) std::toupper(**av);
+				++*av;
+			}
 			++av;
 		}
 	}
+	std::cout << std::endl;
 	return 0;
 }
