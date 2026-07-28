7611fe01b6e267bd95cab77e8bdf9e81abfed133
ansimonn <ansimonn@student.42angouleme.fr>
2026-07-28 18:46:21 +0200

    CPP-01: ex04: finished

--- CPP-01/ex04/main.cpp
+++ CPP-01/ex04/main.cpp
@@ -11,18 +11,17 @@ static void	translateContent(std::string &content, const std::string &toFind, co
 
 int main(const int ac, char **av) {
 	if (ac != 4)
-		std::cout << "Invalid arguments" << std::endl;
+		std::cout << "Invalid arguments: ./replace inFile str1 str2" << std::endl;
 	else {
 		std::string	content;
-		const std::string	fileName = av[1];
-		std::ifstream	inFile;
-		inFile.open(fileName);
+		std::string	inFileName = av[1];
+		std::ifstream	inFile(inFileName.c_str());
 		if (!inFile.is_open()) {
 			std::cout << "Infile not found" << std::endl;
 			return 0;
 		}
-		std::ofstream	outFile;
-		outFile.open(fileName + ".replace");
+		std::string	outFileName = inFileName + ".replace";
+		std::ofstream	outFile(outFileName.c_str());
 		if (!outFile.is_open()) {
 			std::cout << "Cannot create/edit outfile" << std::endl;
 			return 0;
@@ -31,8 +30,9 @@ int main(const int ac, char **av) {
 		std::string	line;
 		while (std::getline(inFile, line)) {
 			translateContent(line, av[2], av[3]);
+			outFile << line;
 			if (!inFile.eof())
-				std::cout << "\n";
+				outFile << std::endl;
 		}
 		inFile.close();
 		outFile.close();
