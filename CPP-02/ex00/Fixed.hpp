2d07d0edb7b391817b45d5a1d36a195ba7d91467
ansimonn <ansimonn@student.42angouleme.fr>
2026-07-29 20:30:30 +0200

    CPP-02: ex00 finished

---
+++ CPP-02/ex00/Fixed.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+
+public:
+	Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	~Fixed();
+
+	int	getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+private:
+	int	value;
+	static const int FRACTIONAL_BITS = 8;
+
+};
+
+#endif
