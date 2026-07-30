3206da651a0615ff92262edf014c9c64b90400df
ansimonn <ansimonn@student.42angouleme.fr>
2026-07-30 19:51:46 +0200

    CPP-02: ex01 finished

---
+++ CPP-02/ex01/Fixed.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iosfwd>
+
+class Fixed
+{
+
+public:
+	Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	~Fixed();
+
+	int	getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int	toInt( void ) const;
+
+private:
+	int	value;
+	static const int FRACTIONAL_BITS = 8;
+
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixedNb);
+
+#endif
