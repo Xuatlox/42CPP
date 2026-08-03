033333ed20887d2137dc110b0bd982e6fb8e6ec9
ansimonn <ansimonn@student.42angouleme.fr>
2026-08-03 16:20:34 +0200

    CPP-02: ex02 finished (min/max functions added)

--- CPP-02/ex02/Fixed.cpp
+++ CPP-02/ex02/Fixed.cpp
@@ -3,32 +3,32 @@
 #include <cmath>
 
 Fixed::Fixed() : value(0) {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int value) {
-	std::cout << "Int constructor called" << std::endl;
+	// std::cout << "Int constructor called" << std::endl;
 	this->value = value << FRACTIONAL_BITS;
 }
 
 Fixed::Fixed(const float value) {
-	std::cout << "Float constructor called" << std::endl;
+	// std::cout << "Float constructor called" << std::endl;
 	this->value = roundf(value * (1 << FRACTIONAL_BITS));
 }
 
 Fixed::Fixed(const Fixed &other) {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other) {
-	std::cout << "Copy assignment operator called" << std::endl;
+	// std::cout << "Copy assignment operator called" << std::endl;
 	this->value = other.getRawBits();
 	return (*this);
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits() const {
@@ -50,4 +50,90 @@ int	Fixed::toInt( void ) const {
 std::ostream &operator<<(std::ostream &os, const Fixed &fixedNb) {
 	os << fixedNb.toFloat();
 	return os;
+}
+
+bool Fixed::operator>(const Fixed &other) const {
+	return this->getRawBits() > other.getRawBits();
+}
+
+bool Fixed::operator<(const Fixed &other) const {
+	return this->getRawBits() < other.getRawBits();
+}
+
+bool Fixed::operator>=(const Fixed &other) const {
+	return this->getRawBits() >= other.getRawBits();
+}
+
+bool Fixed::operator<=(const Fixed &other) const {
+	return this->getRawBits() <= other.getRawBits();
+}
+
+bool Fixed::operator==(const Fixed &other) const {
+	return this->getRawBits() == other.getRawBits();
+}
+
+bool Fixed::operator!=(const Fixed &other) const {
+	return this->getRawBits() != other.getRawBits();
+}
+
+Fixed Fixed::operator+(const Fixed &other) const {
+	return Fixed(this->toFloat() + other.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed &other) const {
+	return Fixed(this->toFloat() - other.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed &other) const {
+	return Fixed(this->toFloat() * other.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed &other) const {
+	return Fixed(this->toFloat() / other.toFloat());
+}
+
+Fixed Fixed::operator++() {
+	++this->value;
+	return *this;
+}
+
+Fixed Fixed::operator++(int) {
+	Fixed temp = *this;
+	++this->value;
+	return temp;
+}
+
+Fixed Fixed::operator--() {
+	--this->value;
+	return *this;
+}
+
+Fixed Fixed::operator--(int) {
+	Fixed temp = *this;
+	--this->value;
+	return temp;
+}
+
+Fixed	&Fixed::min(Fixed &a, Fixed &b) {
+	if (a <= b)
+		return a;
+	return b;
+}
+
+const Fixed	&Fixed::min(const Fixed &a, const Fixed &b) {
+	if (a <= b)
+		return a;
+	return b;
+}
+
+Fixed	&Fixed::max(Fixed &a, Fixed &b) {
+	if (a >= b)
+		return a;
+	return b;
+}
+
+const Fixed	&Fixed::max(const Fixed &a, const Fixed &b) {
+	if (a >= b)
+		return a;
+	return b;
 }
--- CPP-02/ex02/Fixed.hpp
+++ CPP-02/ex02/Fixed.hpp
@@ -19,6 +19,25 @@ public:
 	float	toFloat( void ) const;
 	int	toInt( void ) const;
 
+	bool operator>(const Fixed &other) const;
+	bool operator<(const Fixed &other) const;
+	bool operator>=(const Fixed &other) const;
+	bool operator<=(const Fixed &other) const;
+	bool operator==(const Fixed &other) const;
+	bool operator!=(const Fixed &other) const;
+	Fixed operator+(const Fixed &other) const;
+	Fixed operator-(const Fixed &other) const;
+	Fixed operator*(const Fixed &other) const;
+	Fixed operator/(const Fixed &other) const;
+	Fixed operator++();
+	Fixed operator++(int);
+	Fixed operator--();
+	Fixed operator--(int);
+	static Fixed &min(Fixed &a, Fixed &b);
+	static const Fixed &min(const Fixed &a, const Fixed &b);
+	static Fixed &max(Fixed &a, Fixed &b);
+	static const Fixed &max(const Fixed &a, const Fixed &b);
+
 private:
 	int	value;
 	static const int FRACTIONAL_BITS = 8;
