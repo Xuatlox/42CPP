e9e2151360bbc789239c09ad800ab9c0021eaf27
ansimonn <ansimonn@student.42angouleme.fr>
2026-09-02 12:26:52 +0200

    CPP-03: ex01: finished

--- CPP-04/ex01/main.cpp
+++ CPP-04/ex01/main.cpp
@@ -3,18 +3,14 @@
 
 int main()
 {
-	Animal animals[4] = ;
+	Animal *animals[4] = {new Dog, new Cat, new Dog, new Cat};
 
 	const Animal* dog = new Dog();
 	const Animal* cat = new Cat();
 	delete dog;//should not create a leak
 	delete cat;
 
-	int	i = 0;
-	while (animals[++i])
-	{
+	for (int i = 0; i < 4; i++)
 		delete animals[i];
-	}
-	delete *animals;
 	return 0;
 }
