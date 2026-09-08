f9ad80972de2d5c3df33a79d8ec308e860523267
ansimonn <ansimonn@student.42angouleme.fr>
2026-09-08 17:59:39 +0200

    CPP-01: ex03: check over HumanB weapon added on attack

--- CPP-01/ex03/HumanB.cpp
+++ CPP-01/ex03/HumanB.cpp
@@ -4,7 +4,10 @@
 HumanB::HumanB(const std::string &name) : weapon(NULL), name(name) {}
 
 void	HumanB::attack() {
-	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	if (this->weapon)
+		std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	else
+		std::cout << this->name << " can't attack without a weapon" << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &newWeapon) {
